b184f21da8c3f8f8869da5cd5e11dbb04b64f72c
mshubhr <mshubhr@gmail.com>
2024-04-30 21:18:56 +0530

    Set timer

--- app/src/main/java/com/project/mathgame/GameActivity.kt
+++ app/src/main/java/com/project/mathgame/GameActivity.kt
@@ -2,12 +2,14 @@ package com.project.mathgame
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Locale
 import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
@@ -20,6 +22,10 @@ class GameActivity : AppCompatActivity() {
     private lateinit var ok : Button
     lateinit var next : Button
 
+    private lateinit var timer : CountDownTimer
+    private val starting : Long = 60000
+    var lefttime : Long = starting
+
     private var correct = 0
     private var userscore = 0
     private var userlife = 3
@@ -29,6 +35,7 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_game)
+        supportActionBar!!.title = "Addition"
 
         score = findViewById(R.id.score)
         life = findViewById(R.id.life)
@@ -47,6 +54,7 @@ class GameActivity : AppCompatActivity() {
                     .show()
             }
             else {
+                pauseTimer()
                 val yourans = input.toInt()
                 if (yourans == correct) {
                     userscore += 10
@@ -63,6 +71,8 @@ class GameActivity : AppCompatActivity() {
         }
 
         next.setOnClickListener {
+            pauseTimer()
+            resetTimer()
             gameContinue()
             ans.setText("")
         }
@@ -75,5 +85,41 @@ class GameActivity : AppCompatActivity() {
         val num2 = Random.nextInt(0, 100)
         que.text = "$num1 + $num2"
         correct = num1 + num2
+        startTimer()
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(lefttime, 1000) {
+            override fun onTick(finishedTime: Long) {
+                lefttime = finishedTime
+                updateText()
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onFinish() {
+                pauseTimer()
+                resetTimer()
+                updateText()
+
+                userlife--
+                life.text = userlife.toString()
+                que.text = "Sorry, Time is up!"
+            }
+
+        }.start()
+    }
+
+    fun resetTimer() {
+        lefttime = starting
+        updateText()
+    }
+
+    fun pauseTimer() {
+        timer.cancel()
+    }
+
+    fun updateText() {
+        val remainingTime : Int = (lefttime / 1000).toInt()
+        time.text = String.format(Locale.getDefault(), "%02d", remainingTime)
     }
 }
